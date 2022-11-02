cecf6be8b98603bf7d057acfb96f90a621eb7d02
Дамир <damir.obuhov@mail.ru>
2022-11-02 22:54:05 +0400

    Задача 9

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int year = 2021;
+        if (year % 4 != 0 || year % 100 == 0 && year % 400 != 0) {
+            System.out.println("Год явлется не весокосным");
+        } else {
+            System.out.println("Год явлется весокосным");
+        }
+
+    }
+}
